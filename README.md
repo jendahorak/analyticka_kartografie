5ab94f9eb1a6eaf48eb1aec28b29d31ae50830d6
jenda_horak <484526@muni.cz>
2022-10-05 19:24:16 +0200

    add compostite and images

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Repo pro cvika na Analytickou kartografii
+## CV1
+### Features
+Script takes every raster from \\img folder calculates statistics and kartograficke zaplneni, returns masked tif for every raster and color and 
--- cv_1/analyza_zaplneni.py
+++ cv_1/analyza_zaplneni.py
@@ -3,8 +3,9 @@ import numpy as np
 import os
 import csv
 
-IN_IMG_PATH = 'img'
-OUT_IMG_PATH = f'out_img'
+IN_IMG_PATH = 'analyticka_kartografie\cv_1\img'
+OUT_IMG_PATH = 'analyticka_kartografie\cv_1\out_img'
+CSV_FOLDER_PATH = 'analyticka_kartografie\cv_1\csv_out'
 COLOR_THRESHOLDS = {
     'black': {'min': [0,0,0], 'max': [15,15,15]},
     'grey': {'min': [57,57,57], 'max': [72,72,72]},
@@ -16,7 +17,7 @@ COLOR_THRESHOLDS = {
 
 
 def export_to_csv(data) -> None:
-    csv_file = fr'csv_out\analyza_zaplnenosti_map.csv'
+    csv_file = os.path.join(CSV_FOLDER_PATH, 'analyza_zaplnenosti_map.csv')
     with open(csv_file, 'w', newline='') as csv_file:
         fieldnames = data[0].keys()
         writer = csv.DictWriter(csv_file, fieldnames)
@@ -29,7 +30,7 @@ def write_img(img, img_name)-> None:
     '''
     Writes out masked tif for given color
     '''
-    cv2.imwrite(os.path.join(OUT_IMG_PATH,f'{img_name}'), img)
+    cv2.imwrite(os.path.join(OUT_IMG_PATH,f'{img_name}.tif'), img)
     pass
 
 def calculate_threshold_vals(img_data, img, pixels_all, img_name)-> dict:
@@ -39,6 +40,7 @@ def calculate_threshold_vals(img_data, img, pixels_all, img_name)-> dict:
     update_img_data = img_data
     color_sum = 0
     relative_color_counts = {}
+    masks = []
 
     for color, values in COLOR_THRESHOLDS.items():
         color_min = np.array(values.get('min')[::-1])
@@ -46,12 +48,16 @@ def calculate_threshold_vals(img_data, img, pixels_all, img_name)-> dict:
         # print(color_min, color_max)
 
         image_mask = cv2.inRange(img, color_min, color_max)
-        write_img(image_mask, f'{img_name}_{color}')
-    
+        masks.append(image_mask)
+        # write mask for each color
+        # write_img(image_mask, f'{img_name}_{color}')
+
         count_pixels = cv2.countNonZero(image_mask)
+
+        if color == 'red' or color == 'brown':
+            count_pixels /=  4
+
         color_sum += count_pixels
-
-
 
         update_img_data[color] = count_pixels
         relative_color_counts[color] = (round((count_pixels / pixels_all)  * 100, 2))
@@ -60,7 +66,12 @@ def calculate_threshold_vals(img_data, img, pixels_all, img_name)-> dict:
     for c, v in relative_color_counts.items():
         update_img_data[f'{c}_relative'] = v
     
-    update_img_data['sum_relative'] = round((color_sum / pixels_all) * 100, 2)
+    # write composite mask for all colors
+    final_mask = masks[0]
+    for mask in masks[1:]:
+        final_mask = final_mask | mask
+    write_img(final_mask, f'{img_name}_composite')
+
 
     return update_img_data
 
@@ -74,14 +85,12 @@ def get_image_statistics(images_to_compute:list) -> list:
 
         img_data = {}
 
-        img_name = img.split('\\', 1)[1]
+        img_name = img.rsplit('\\', 1)[1]
         dimensions = str(loaded_img.shape)
         height = loaded_img.shape[0]
         width = loaded_img.shape[1]
         channels = loaded_img.shape[2]
         pixels_all = height*width
-        # out.append({'img_name': img_name, 'pixels_all': pixels_all,
-        #            'brown': brown_part, 'red': red_part, 'black': black_part})
 
         img_data = {
             'raster_name': img_name,
@@ -100,6 +109,7 @@ def main():
     Loads all images from \\img folder, exports results to csv
     '''
     images_to_compute = [os.path.join(IN_IMG_PATH, img_p)for img_p in os.listdir(IN_IMG_PATH)]
+    get_image_statistics(images_to_compute)
     export_to_csv(get_image_statistics(images_to_compute)) 
 pass
 
